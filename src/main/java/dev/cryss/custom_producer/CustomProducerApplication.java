5c3ad9326d98f368ec25ed68251dc372ec1a890a
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-06-04 06:55:29 -0300

    iniciatial project.

---
+++ src/main/java/dev/cryss/custom_producer/CustomProducerApplication.java
@@ -0,0 +1,13 @@
+package dev.cryss.custom_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cryss/custom_producer/CustomProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cryss.custom_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
