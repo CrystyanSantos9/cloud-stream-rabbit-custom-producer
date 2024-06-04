13985d5a3a285e1818b9b6dce0eae5170e4cf5d8
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-06-04 10:17:15 -0300

    iniciatial project.

--- src/main/java/dev/cryss/custom_producer/CustomProducerApplication.java
+++ src/main/java/dev/cryss/custom_producer/CustomProducerApplication.java
@@ -1,13 +1,20 @@
 package dev.cryss.custom_producer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class CustomProducerApplication {
+public class CustomProducerApplication implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println ("começou");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomProducerApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/dev/cryss/custom_producer/message/CustomSource.java
@@ -0,0 +1,7 @@
+package dev.cryss.custom_producer.message;
+
+public interface CustomSource {
+
+
+
+}
